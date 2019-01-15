ba1daff667a077bf3e047e2cadfda99d3c51410c
epictrust <epictrust72404e@users.noreply.github.com>
2019-01-15 05:09:13 -0800

    Add POST for new users as well as test cases for users

--- server/models/user.js
+++ server/models/user.js
@@ -69,10 +69,31 @@ var UserSchema = new mongoose.Schema({
     });
   };
 
-UserSchema.pre('save', function (next) {
-  var user = this;
+  UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
 
-  if (user.isModified('password')) {
+    return User.findOne({email}).then((user) => {
+      if (!user) {
+        return Promise.reject();
+      }
+
+      return new Promise((resolve, reject) => {
+        // Use bcrypt.compare to compare password and user.password
+        bcrypt.compare(password, user.password, (err, res) => {
+          if (res) {
+            resolve(user);
+          } else {
+            reject();
+          }
+        });
+      });
+    });
+  };
+
+  UserSchema.pre('save', function (next) {
+    var user = this;
+
+    if (user.isModified('password')) {
       bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(user.password, salt, (err, hash) => {
           user.password = hash;
--- server/server.js
+++ server/server.js
@@ -121,24 +121,21 @@ app.post('/users', (req, res) => {
   })
 });
 
- var authenticate = (req, res, next) => {
-   var token = req.header('x-auth');
-
-   User.findByToken(token).then((user) => {
-     if (!user) {
-       return Promise.reject();
-     }
-
-     req.user = user;
-     req.token = token;
-     next();
-   }). catch((e) => {
-     res.status(401).send();
-   });
- };
-
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+// POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
